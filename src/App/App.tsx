f47c53f9b2607430fb8bd2c0e7d43e13ee961c43
marzie <marziemirjalili.78@gmail.com>
2023-10-13 19:55:14 +0200

    fix(app): refactor city form

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,33 +1,21 @@
-import React from "react"
-
-import { Weather } from "./Weather"
-
-import type { FC } from "react"
-
-import "./app.css"
+import { useState } from "react";
+import { Weather } from "./Weather";
+import type { FC } from "react";
+import "./app.css";
 
 export const App: FC = () => {
-    const [ input, setInput ] = 
-        React.useState( null as unknown as string )
-
-    const [ city, setCity ] =
-        React.useState( null as unknown as string )
-
-    const doStuff = ( event: any ) => 
-        setInput( event.target.value )
-
-    console.log( input )
-    
-    return  <>
-                <input 
-                    role="search"
-                    type="text" 
-                    value={ input as unknown as string } 
-                    onChange={ doStuff }  />
-
-                <button onClick={ () => setCity( input ) } >
-                    Show Weather
-                </button>
-
-                < Weather city={ city } />
-            </> }
+  const [cityInputValue, setCityInputValue] = useState("");
+  const [city, setCity] = useState("");
+  return (
+    <>
+      <input
+        role="search"
+        type="text"
+        value={cityInputValue}
+        onChange={(event) => setCityInputValue(event.target.value)}
+      />
+      <button onClick={() => setCity(cityInputValue)}>Show Weather</button>
+      <Weather city={city} />
+    </>
+  );
+};

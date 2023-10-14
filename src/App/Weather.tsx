ac75fe26a7458fb31ab84b03c7b942a7ab0e98f6
marzie <marziemirjalili.78@gmail.com>
2023-10-14 10:02:58 +0200

    fix(weather): clean weather file

--- src/App/Weather.tsx
+++ src/App/Weather.tsx
@@ -1,47 +1,67 @@
-import React from "react"
+import { FC, useCallback, useEffect, useState } from "react";
+import { CityType, WeatherType } from "./Types";
 
-const Weather = ( props: any ) => {
-    const [ info, setInfo ] = React.useState( {
-        city: null,
-        temp: null,
-        icon: null, } )
+type WeatherPropsType = {
+  city?: string;
+};
 
-    console.log( "info" + JSON.stringify( info ) )
+export const Weather: FC<WeatherPropsType> = ({ city }) => {
+  const [cityWeatherInfo, setCityWeatherInfo] = useState<WeatherType>();
+  const [isLoadingWeatherInfo, setIsLoadingWeatherInfo] = useState(false);
 
-    const isPending = React.useRef( false )
+  const getCoordinationOfCity = async (city: string) => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=4c4f0b1876954338598a7be96c66527b`
+      );
+      const data = await res.json();
+      return data[0] as CityType;
+    } catch (error) {
+      alert(error);
+    }
+  };
 
-    React.useLayoutEffect( () => {
-        if ( props.city !== null || isPending.current === false ) {
+  const getWeatherOfCity = useCallback(async (city: string) => {
+    try {
+      setIsLoadingWeatherInfo(true);
+      const coordination = await getCoordinationOfCity(city);
+      if (!coordination) {
+        setIsLoadingWeatherInfo(false);
+        return;
+      }
+      const weatherRes = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coordination.lat}&lon=${coordination.lon}&units=metric&appid=4c4f0b1876954338598a7be96c66527b`
+      );
+      const weather: WeatherType = await weatherRes.json();
+      setCityWeatherInfo(weather);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setIsLoadingWeatherInfo(false);
+    }
+  }, []);
 
-        isPending.current = true
+  useEffect(() => {
+    if (city) {
+      getWeatherOfCity(city);
+    }
+  }, [city, getWeatherOfCity]);
 
-        fetch( `https://api.openweathermap.org/geo/1.0/direct?q=${ props.city }&appid=4c4f0b1876954338598a7be96c66527b` )
-            .then( res => res.json() )
-            .then( data => {
-                console.log( data )
-
-                fetch( `https://api.openweathermap.org/data/2.5/weather?lat=${ data[ 0 ].lat }&lon=${ data[ 0 ].lon }&units=metric&appid=4c4f0b1876954338598a7be96c66527b` )
-                    .then( res => res.json() )
-                    .then( data => {
-                        console.log( data )
-                        
-                        setInfo( {
-                            city: data.name,
-                            temp: data.main.temp,
-                            icon: `https://openweathermap.org/img/wn/${ data.weather[ 0 ].icon }@2x.png` as any, } ) 
-                        
-                        isPending.current = false } ) } ) } }, [ props.city ] )
-
-    return  <>
-                <h1>
-                    { info.city }
-                </h1>
-
-                <p>
-                    { info.temp && ~~ info.temp } Celcius
-                </p>
-
-                <img src={ info.icon as unknown as string } alt="Icon" />
-            </> }
-
-export { Weather }
+  if (isLoadingWeatherInfo) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      {!!cityWeatherInfo && (
+        <>
+          <h1>{cityWeatherInfo.name}</h1>
+          <p>{Math.floor(cityWeatherInfo.main.temp)} Celcius</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${cityWeatherInfo.weather[0].icon}@2x.png`}
+            alt="Icon"
+          />
+        </>
+      )}
+    </>
+  );
+};
